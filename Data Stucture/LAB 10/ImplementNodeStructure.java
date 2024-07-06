954dc44958b3817ef63eea3828ed0e7af150796f
neelgohelh <neelgohel8887@gmail.com>
2024-07-06 19:37:21 +0530

    06-07-2024

---
+++ Data Stucture/LAB 10/ImplementNodeStructure.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ImplementNodeStructure {
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+    
+        Node node = new Node(5);
+        node.display();
+
+    }
+}
+    class Node {
+        int info;
+        Node link;
+
+        Node(int data) {
+            this.info = data;
+            this.link = null;
+        }
+
+        public Node(int info, Node link) {
+            this.info = info;
+            this.link = link;
+        }
+
+        public void display() {
+            System.out.println("Data = " + this.info); // Print the data of the node
+        }
+    }
+
