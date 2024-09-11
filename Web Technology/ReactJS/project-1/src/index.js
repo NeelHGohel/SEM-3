b70facafa861a9e6bc7afc5adc22096ede9e2a97
neelgohelh <neelgohel8887@gmail.com>
2024-09-11 11:44:40 +0530

    11-09-2024

--- Web Technology/ReactJS/project-1/src/index.js
+++ Web Technology/ReactJS/project-1/src/index.js
@@ -2,29 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-function Abc(props){
-  let temp = "";
-
-  for(let i = 0 ; i < props.count ; i++){
-    temp += props.by;
-  }
-  return (<h1>{ temp }</h1>);
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-  <>
-      <h1>Hello</h1>
-      <Abc count = "5" by = "@"/>
-      <h2>World</h2>
-      <Abc count = "5" by = "@"/>
-      <h3>From</h3>
-      <Abc count = "5" by = "@"/>
-      <h4>React</h4>
-      <Abc count = "5" by = "@"/>
-  </>
-
-  
-
-);
